2364d0838c4c5dbe293bd81c486ff78302d9c753
(Quentin Milhomme) <milhommequentin@yahoo.fr>
2024-08-30 19:49:13 +0200

    add index dice controller et seed

--- app/controllers/dices_controller.rb
+++ app/controllers/dices_controller.rb
@@ -1,5 +1,5 @@
 class DicesController < ApplicationController
-
-
-
+  def index
+    @dices = Dice.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Dice.create(number_of_faces: 4)
+Dice.create(number_of_faces: 6)
+Dice.create(number_of_faces: 8)
+Dice.create(number_of_faces: 10)
+Dice.create(number_of_faces: 12)
+Dice.create(number_of_faces: 20)
